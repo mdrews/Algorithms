b026925fa2065370675494b7893f79f599eccba7
mdrews <matthewdrews@gmail.com>
2020-01-01 22:33:02 -0500

    add generic chart

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ColumnChart from './ColumnChart';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <ColumnChart />
       </header>
     </div>
   );
---
+++ src/ColumnChart.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Chart from 'react-google-charts';
+
+const data = [
+  ["Element", "Density", { role: "style" }],
+  ["Copper", 8.94, "#b87333"], // RGB value
+  ["Silver", 10.49, "silver"], // English color name
+  ["Gold", 19.3, "gold"],
+  ["Platinum", 21.45, "color: #e5e4e2"] // CSS-style declaration
+]
+
+export default function ColumnChart() {
+  return (<div>
+    <Chart
+      chartType="ColumnChart"
+      width="100%"
+      height="100%"
+      data={data}
+    />
+  </div>);
+}
