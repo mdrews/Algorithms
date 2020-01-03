d248e9eb2e5b2aa94b84ecce8b0aaef098803638
mdrews <matthewdrews@gmail.com>
2020-01-02 22:43:41 -0500

    move data from chart to app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import ColumnChart from './ColumnChart';
 
+const data = [
+  ["Element", "Density", { role: "style" }],
+  ["Copper", 8.94, "#b87333"], // RGB value
+  ["Silver", 10.49, "silver"], // English color name
+  ["Gold", 19.3, "gold"],
+  ["Platinum", 21.45, "color: #e5e4e2"] // CSS-style declaration
+]
+
 function App() {
   return (
     <div className="App">
@@ -19,7 +27,7 @@ function App() {
         >
           Learn React
         </a>
-        <ColumnChart />
+        <ColumnChart data={data}/>
       </header>
     </div>
   );
