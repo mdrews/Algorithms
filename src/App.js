c40095b33ed6a4ae832acc4bc88f9577094824d2
mdrews <matthewdrews@gmail.com>
2020-01-02 23:53:47 -0500

    add random shuffle to data array

--- src/App.js
+++ src/App.js
@@ -16,6 +16,21 @@ for(let x = 0; x < COLUMNS; x++) {
   data.push([x.toString(), x, "blue"]);
 }
 
+
+
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+  }
+}
+
+
+
+shuffleArray(data);
+
 data.unshift(["Element", "Density", { role: "style" }]);
 
 function App() {
